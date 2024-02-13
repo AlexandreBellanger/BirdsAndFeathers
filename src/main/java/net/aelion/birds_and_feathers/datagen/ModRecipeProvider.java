691656329d715fe9e2c61e64edf96554287149cf
Aelion <alexandre.bellanger12@gmail.com>
2024-02-13 16:50:24 +0100

    Added recipes for beds

--- src/main/java/net/aelion/birds_and_feathers/datagen/ModRecipeProvider.java
+++ src/main/java/net/aelion/birds_and_feathers/datagen/ModRecipeProvider.java
@@ -8,9 +8,12 @@ import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -20,6 +23,25 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(@NotNull RecipeOutput pRecipeOutput) {
+        List<Block> beds = Arrays.asList(
+                Blocks.WHITE_BED,
+                Blocks.LIGHT_GRAY_BED,
+                Blocks.GRAY_BED,
+                Blocks.BLACK_BED,
+                Blocks.BROWN_BED,
+                Blocks.RED_BED,
+                Blocks.ORANGE_BED,
+                Blocks.YELLOW_BED,
+                Blocks.LIME_BED,
+                Blocks.GREEN_BED,
+                Blocks.CYAN_BED,
+                Blocks.LIGHT_BLUE_BED,
+                Blocks.BLUE_BED,
+                Blocks.PURPLE_BED,
+                Blocks.MAGENTA_BED,
+                Blocks.PINK_BED
+        );
+
         for (int i = 0; i<16; ++i){
             Block featherBlock = ModBlocks.FEATHER_BLOCKS.get(i).get();
             Item feather = i==0 ? Items.FEATHER : ModItems.COLORED_FEATHERS.get(i-1).get();
@@ -36,6 +58,8 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                     .requires(featherBlock)
                     .unlockedBy(getHasName(featherBlock), has(featherBlock))
                     .save(pRecipeOutput);
+
+            bedFromPlanksAndWool(pRecipeOutput, beds.get(i), featherBlock);
         }
     }
 }
