59119fe2c694d847470d7d8ec5e5a12536ab7eba
Aelion <alexandre.bellanger12@gmail.com>
2024-02-26 14:59:46 +0100

    Cleaner ParrotLootReplacer

--- src/main/java/net/aelion/birds_and_feathers/loot/ParrotLootReplacer.java
+++ src/main/java/net/aelion/birds_and_feathers/loot/ParrotLootReplacer.java
@@ -5,26 +5,19 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.aelion.birds_and_feathers.items.ModItems;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.commands.data.DataAccessor;
-import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Parrot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.common.loot.LootModifier;
+import net.neoforged.neoforge.registries.DeferredItem;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
 import java.util.function.Supplier;
 
 public class ParrotLootReplacer extends LootModifier {
@@ -46,51 +39,40 @@ public class ParrotLootReplacer extends LootModifier {
             }
         }
 
-        // I don't know if this is needed
+        // I don't know if this check is needed
         if (!context.hasParam(LootContextParams.THIS_ENTITY)) {
             return generatedLoot;
         }
-
         Entity entity = context.getParam(LootContextParams.THIS_ENTITY);
-        SynchedEntityData entityData = entity.getEntityData();
-
-        // Default parrot variant has red feathers
-        Item correct_feather = ModItems.RED_FEATHER.get();
-
-        // I feel like this is not at all how it should be done
-        for ( SynchedEntityData.DataValue<?> dataValue: Objects.requireNonNull(entityData.getNonDefaultValues())){
-            if (dataValue.id() == 19) {
-                int parrotVariantId = (int) dataValue.value();
-
-                if (parrotVariantId == Parrot.Variant.RED_BLUE.getId()) {
-                    // Note: this is actually never reached, as RED_BLUE is the default variant
-                    correct_feather = ModItems.RED_FEATHER.get();
-                } else if (parrotVariantId == Parrot.Variant.BLUE.getId()) {
-                    correct_feather = ModItems.BLUE_FEATHER.get();
-                } else if (parrotVariantId == Parrot.Variant.GRAY.getId()) {
-                    correct_feather = ModItems.LIGHT_GRAY_FEATHER.get();
-                } else if (parrotVariantId == Parrot.Variant.YELLOW_BLUE.getId()) {
-                    // I may change this to cyan later
-                    correct_feather = ModItems.LIGHT_BLUE_FEATHER.get();
-                } else if (parrotVariantId == Parrot.Variant.GREEN.getId()) {
-                    correct_feather = ModItems.LIME_FEATHER.get();
-                } else {
-                    // Unknown parrot variant, should not mess with it
-                    return generatedLoot;
-                }
-
-                break;
-            }
+        // Should I just cast without checking: are the checks already done with the LootItemConditions ?
+        if (!(entity instanceof Parrot parrot)) {
+            return generatedLoot;
         }
 
-        ItemStack coloredFeatherItemStack = new ItemStack(correct_feather);
+        DeferredItem<Item> correct_feather = switch (parrot.getVariant()) {
+            case RED_BLUE -> ModItems.RED_FEATHER;
+            case BLUE -> ModItems.BLUE_FEATHER;
+            case GREEN -> ModItems.LIME_FEATHER;
+            case YELLOW_BLUE -> ModItems.LIGHT_BLUE_FEATHER;
+            case GRAY -> ModItems.LIGHT_GRAY_FEATHER;
+            // A warning says default branch is unnecessary
+            // but this could maybe prevent a crash if mojang (or another mod ?) adds more parrots variants
+            default -> null;
+        };
+        if (correct_feather == null) {
+            return generatedLoot;
+        }
 
-        for (ItemStack itemStack: generatedLoot) {
-            if (itemStack.getItem() == Items.FEATHER){
+        ItemStack coloredFeatherItemStack = new ItemStack(correct_feather.get());
+
+        for (ItemStack vanillaLootItemStack: generatedLoot) {
+            if (vanillaLootItemStack.getItem() == Items.FEATHER){
                 // Changing the number of feathers to match the number of vanilla feathers that would normally drop
-                coloredFeatherItemStack.setCount(itemStack.getCount());
+                coloredFeatherItemStack.setCount(vanillaLootItemStack.getCount());
                 // Removing the vanilla feather from the loot
-                generatedLoot.remove(itemStack);
+                generatedLoot.remove(vanillaLootItemStack);
+                // Even if it was useful, I would not trust resuming the list iteration after removing an entry
+                break;
             }
         }
 
